e6283ec320eda235af2f3e39b18d542b2a6584b2
Renan Souza <renantrsouza@gmail.com>
2021-10-22 12:38:41 -0400

    add fetch api github

--- main.js
+++ main.js
@@ -7,6 +7,7 @@ const linksSocialMedia = {
 }
 
 function showMeUser(){
+    getGitHubProfileInfos()
     const li = document.querySelectorAll('ul li');
     for(let media of li){
         const socialMedia = media.dataset.social
@@ -15,3 +16,19 @@ function showMeUser(){
 }
 
 showMeUser()
+
+function getGitHubProfileInfos(){
+    const url = `https://api.github.com/users/${linksSocialMedia.github}`
+    console.log(url)
+    fetch(url)
+    .then(response=>response.json())
+    .then(data => {
+        const link = `https://github.com/${data.login}`
+
+        imagem.src = data.avatar_url
+        nameUser.innerHTML = data.name
+        aLink.href = data.html_url
+        userLogin.innerHTML = data.login
+        bio.innerHTML = data.bio
+    });
+}
